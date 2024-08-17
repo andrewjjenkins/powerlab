4d55f9178cc1a48150155a99f541539c8bd8d07f
Andrew Jenkins <andrewjjenkins@gmail.com>
2024-08-17 20:36:22 +0000

    Get raw sensor data

--- pkg/serve/server/servers.go
+++ pkg/serve/server/servers.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -34,6 +35,8 @@ func (ss *ServerServer) wrap(f func(ss *ServerServer, c *gin.Context)) gin.Handl
 	}
 }
 
+type SensorsRawResponse interface{}
+
 func (ss *ServerServer) RegisterApiServer(r *gin.RouterGroup) {
 	// swagger:route GET /api/servers getServers
 	//
@@ -45,6 +48,29 @@ func (ss *ServerServer) RegisterApiServer(r *gin.RouterGroup) {
 	//   Response:
 	//     200: body:serversResponse
 	r.GET("/servers", ss.wrap(GetServers))
+
+	// swagger:route GET /api/server/{name} getServerByName
+	//
+	// Returns a server
+	//
+	//   Produces:
+	//   - application/json
+	//
+	//   Response:
+	//     200: body:ServerResponse
+	r.GET("/servers/:name", ss.wrap(GetServerByName))
+
+	// swagger:route GET /api/server/{name}/sensorsRaw getServerSensorsRaw
+	//
+	// Returns the sensors from a server as raw JSON
+	//
+	//   Produces:
+	//   - application/json
+	//
+	//   Response:
+	//     200: body:SensorsRawResponse
+	r.GET("/servers/:name/sensorsRaw", ss.wrap(getServerSensorsRaw))
+
 }
 
 func GetServers(ss *ServerServer, c *gin.Context) {
@@ -58,4 +84,32 @@ func GetServers(ss *ServerServer, c *gin.Context) {
 		})
 	}
 	c.JSON(http.StatusOK, res)
+}
+
+func GetServerByName(ss *ServerServer, c *gin.Context) {
+	name := c.Param("name")
+	server, ok := ss.manager.Servers[name]
+	if !ok {
+		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("no server %s", name))
+		return
+	}
+	c.JSON(http.StatusOK, ServerResponse{
+		Name:        server.Name(),
+		PowerStatus: 1,
+		PowerWatts:  313.2,
+	})
+}
+
+func getServerSensorsRaw(ss *ServerServer, c *gin.Context) {
+	name := c.Param("name")
+	server, ok := ss.manager.Servers[name]
+	if !ok {
+		c.AbortWithError(http.StatusBadRequest, fmt.Errorf("no server %s", name))
+		return
+	}
+	rawSensorOut, err := server.GetSensorsRaw()
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, fmt.Errorf("error getting sensor data: %v", err))
+	}
+	c.JSON(http.StatusOK, rawSensorOut)
 }
