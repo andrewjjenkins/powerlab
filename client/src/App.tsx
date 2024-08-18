674936c469a8b353f355b0d4049e82d0556c76eb
Andrew Jenkins <andrewjjenkins@gmail.com>
2024-08-18 04:22:04 +0000

    Add tailwind and daisyui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import Servers from './Servers';
 import './App.css';
+
+function NewApp() {
+  return (
+    <div className="min-h-full">
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Powerlab Servers</h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {<Servers></Servers>}
+        </div>
+      </main>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -23,4 +41,4 @@ function App() {
   );
 }
 
-export default App;
+export default NewApp;
