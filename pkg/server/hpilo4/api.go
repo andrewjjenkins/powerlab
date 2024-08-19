eb0fb070160cb16fb7f313db1a58eadd22d224e6
Andrew Jenkins <andrewjjenkins@gmail.com>
2024-08-19 23:56:03 +0000

    Read temps from HP iLO4

--- pkg/server/hpilo4/api.go
+++ pkg/server/hpilo4/api.go
@@ -2,6 +2,9 @@ package hpilo4
 
 import (
 	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/cookiejar"
 	"time"
@@ -19,11 +22,42 @@ func (api *Api) Name() string {
 	return api.ServerAddr
 }
 
-/*
 func (api *Api) NewRequest(method, path string, body io.Reader) (*http.Request, error) {
-
+	u := "https://" + api.ServerAddr + path
+	return http.NewRequest(method, u, body)
 }
-*/
+
+func (api *Api) Do(req *http.Request) (*http.Response, error) {
+	return api.client.Do(req)
+}
+
+func (api *Api) Get(path string) (*http.Response, error) {
+	r, err := api.NewRequest("GET", path, nil)
+	if err != nil {
+		return nil, err
+	}
+	return api.Do(r)
+}
+
+func (api *Api) GetJson(path string, obj interface{}) error {
+	res, err := api.Get(path)
+	if err != nil {
+		return fmt.Errorf("failed getting %s: %v", path, err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed getting %s: %v", path, res.StatusCode)
+	}
+	contentType := res.Header.Get("content-type")
+	if contentType != "application/json" && contentType != "application/x-javascript" {
+		return fmt.Errorf("unexpected content type for %s: %s", path, contentType)
+	}
+
+	err = json.NewDecoder(
+		io.LimitReader(res.Body, 1024*1024*10),
+	).Decode(obj)
+	return err
+}
 
 func newHttpClientInternal(insecureSsl bool) *http.Client {
 	transport := http.Transport{}
--- pkg/server/hpilo4/sensors.go
+++ pkg/server/hpilo4/sensors.go
@@ -1,11 +1,65 @@
 package hpilo4
 
-import "github.com/andrewjjenkins/powerlab/pkg/model"
+import (
+	"fmt"
+
+	"github.com/andrewjjenkins/powerlab/pkg/model"
+)
+
+type TemperatureSensorResponse struct {
+	Label          string `json:"label"`
+	XPosition      int    `json:"xposition"`
+	YPosition      int    `json:"yposition"`
+	Location       string `json:"location"`
+	Status         string `json:"status"`
+	CurrentReading int    `json:"currentreading"`
+	Caution        int    `json:"caution"`
+	Critical       int    `json:"critical"`
+	TempUnit       string `json:"temp_unit"`
+}
+
+type TemperatureSensorsResponse struct {
+	HostPowerState string                      `json:"hostpwr_state"`
+	Temperatures   []TemperatureSensorResponse `json:"temperature"`
+}
+
+func (api *Api) getSensorsInternal() (*TemperatureSensorsResponse, error) {
+	var temps TemperatureSensorsResponse
+	err := api.GetJson("/json/health_temperature", &temps)
+	if err != nil {
+		return nil, err
+	}
+	return &temps, nil
+}
 
 func (api *Api) GetSensors() (*model.ServerSensorReadings, error) {
-	return &model.ServerSensorReadings{}, nil
+	temps, err := api.getSensorsInternal()
+	if err != nil {
+		return nil, err
+	}
+
+	tempsByName := make(map[string]TemperatureSensorResponse)
+	for _, t := range temps.Temperatures {
+		if _, ok := tempsByName[t.Label]; ok {
+			return nil, fmt.Errorf("duplicate temp sensor %s", t.Label)
+		}
+		tempsByName[t.Label] = t
+	}
+
+	for _, l := range []string{"02-CPU 1", "12-PS 2 Inlet"} {
+		if _, ok := tempsByName[l]; !ok {
+			return nil, fmt.Errorf("no temp sensor %s", l)
+		}
+	}
+
+	return &model.ServerSensorReadings{
+		CpuTemp:     float64(tempsByName["02-CPU 1"].CurrentReading),
+		ChassisTemp: float64(tempsByName["12-PS 2 Inlet"].CurrentReading),
+		FanSpeed:    0.0,
+		PowerWatts:  0.0,
+	}, nil
 }
 
 func (api *Api) GetSensorsRaw() (interface{}, error) {
-	return nil, nil
+	return api.getSensorsInternal()
 }
