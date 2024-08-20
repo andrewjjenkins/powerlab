c2e6a9d398db42b1575c254eadb6d63ded587e60
Andrew Jenkins <andrewjjenkins@gmail.com>
2024-08-20 00:12:20 +0000

    Read power supplies from iLO 4 to get system watts

--- pkg/server/hpilo4/sensors.go
+++ pkg/server/hpilo4/sensors.go
@@ -23,7 +23,7 @@ type TemperatureSensorsResponse struct {
 	Temperatures   []TemperatureSensorResponse `json:"temperature"`
 }
 
-func (api *Api) getSensorsInternal() (*TemperatureSensorsResponse, error) {
+func (api *Api) getTemperaturesInternal() (*TemperatureSensorsResponse, error) {
 	var temps TemperatureSensorsResponse
 	err := api.GetJson("/json/health_temperature", &temps)
 	if err != nil {
@@ -32,8 +32,48 @@ func (api *Api) getSensorsInternal() (*TemperatureSensorsResponse, error) {
 	return &temps, nil
 }
 
+type PowerSupplyResponse struct {
+	Unhealthy        int    `json:"unhealthy"`
+	Enabled          int    `json:"enabled"`
+	Mismatch         int    `json:"mismatch"`
+	PsBay            int    `json:"ps_bay"`
+	PsPresent        string `json:"ps_present"`
+	PsCondition      string `json:"ps_condition"`
+	PsErrorCode      string `json:"ps_error_code"`
+	PsIpduCapable    string `json:"ps_ipdu_capable"`
+	PsHotplugCapable string `json:"ps_hotplug_capable"`
+	PsModel          string `json:"ps_model"`
+	PsSpare          string `json:"ps_spare"`
+	PsSerialNum      string `json:"ps_serial_num"`
+	PsMaxCapWatts    int    `json:"ps_max_cap_watts"`
+	PsFwVer          string `json:"ps_fw_ver"`
+	PsInputVolts     int    `json:"ps_input_volts"`
+	PsOutputWatts    int    `json:"ps_output_watts"`
+	Average          int    `json:"avg"`
+	Max              int    `json:"max"`
+	Supply           bool   `json:"supply"`
+	Bbu              bool   `json:"bbu"`
+	Charge           int    `json:"charge"`
+	Age              int    `json:"age"`
+	BatteryHealth    int    `json:"battery_health"`
+}
+
+type PowerSuppliesResponse struct {
+	PresentPowerReading int                   `json:"present_power_reading"`
+	Supplies            []PowerSupplyResponse `json:"supplies"`
+}
+
+func (api *Api) getPowerInternal() (*PowerSuppliesResponse, error) {
+	var powers PowerSuppliesResponse
+	err := api.GetJson("/json/power_supplies", &powers)
+	if err != nil {
+		return nil, err
+	}
+	return &powers, nil
+}
+
 func (api *Api) GetSensors() (*model.ServerSensorReadings, error) {
-	temps, err := api.getSensorsInternal()
+	temps, err := api.getTemperaturesInternal()
 	if err != nil {
 		return nil, err
 	}
@@ -52,14 +92,36 @@ func (api *Api) GetSensors() (*model.ServerSensorReadings, error) {
 		}
 	}
 
+	powerSupplies, err := api.getPowerInternal()
+	if err != nil {
+		return nil, err
+	}
+
 	return &model.ServerSensorReadings{
 		CpuTemp:     float64(tempsByName["02-CPU 1"].CurrentReading),
 		ChassisTemp: float64(tempsByName["12-PS 2 Inlet"].CurrentReading),
 		FanSpeed:    0.0,
-		PowerWatts:  0.0,
+		PowerWatts:  float64(powerSupplies.PresentPowerReading),
 	}, nil
 }
 
+type rawResponse struct {
+	Temperatures  *TemperatureSensorsResponse `json:"temperatures"`
+	PowerSupplies *PowerSuppliesResponse      `json:"power_supplies"`
+}
+
 func (api *Api) GetSensorsRaw() (interface{}, error) {
-	return api.getSensorsInternal()
+	temperatureResp, err := api.getTemperaturesInternal()
+	if err != nil {
+		return nil, err
+	}
+	powerSuppliesResponse, err := api.getPowerInternal()
+	if err != nil {
+		return nil, err
+	}
+
+	return &rawResponse{
+		Temperatures:  temperatureResp,
+		PowerSupplies: powerSuppliesResponse,
+	}, nil
 }
